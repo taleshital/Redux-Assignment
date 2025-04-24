f8c593f568525d86942ccb34d6ce1ab8d1e9676e
taleshital <taleshital28@gmail.com>
2025-04-24 15:46:59 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Provider } from "react-redux";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Counter from "./components/Counter";
+import store from "./store";
+import AddTodos from "./components/AddTodos";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import RecAddTodos from "./components/RecAddTodos";
+import Home from "./components/Home";
+
+function App() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}>
+                        <Route path="/" element={<Counter />}></Route>
+                        <Route path="/task2" element={<AddTodos />}></Route>
+                        <Route
+                            path="/task3"
+                            element={
+                                <RecoilRoot>
+                                    <RecAddTodos />
+                                </RecoilRoot>
+                            }
+                        ></Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    );
+}
+
+export default App;
---
+++ src/components/AddTodos.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ShowTodos from "./showTodos";
+import { useDispatch } from 'react-redux'
+import { addTodo } from "../Features/todoSlice";
+const AddTodos = () => {
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+    const handleSubmit = () => {
+        if (input.trim()) {
+
+            dispatch(addTodo(input))
+            alert('todo added !!!')
+            setInput('')
+        }
+    }
+    return (
+        <>
+            <div className="container row px-5 mt-5">
+                <h1 className="mb-5">Todos List</h1>
+                <div className="col-8">
+                    <input
+                        type="text"
+                        placeholder="Enter your todo..."
+                        className="form-control"
+                        onChange={(e) => setInput(e.target.value)}
+                        value={input}
+                    />
+                </div>
+                <div className="col-2">
+                    <button onClick={handleSubmit} className="btn btn-primary">Add Todo</button>
+                </div>
+                <ShowTodos />
+            </div>
+        </>
+    );
+};
+
+export default AddTodos;
